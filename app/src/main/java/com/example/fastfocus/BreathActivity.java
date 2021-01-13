9642d7624979cb89924e4b5018fbd9c2646ef349
mousqtr <49991720+mousqtr@users.noreply.github.com>
2021-01-13 03:58:18 +0100

    Change strings

--- app/src/main/java/com/example/fastfocus/BreathActivity.java
+++ app/src/main/java/com/example/fastfocus/BreathActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
